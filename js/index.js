4207b2c2bc0897d4f12a5f8a544b52e4acb059e2
igaliva <96495282+igaliva@users.noreply.github.com>
2022-01-24 13:00:59 +0200

    updated

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Header's Page
+// Header
 function buildHeader () {
     const header = document.createElement('Header');
     header.classList.add('header');
@@ -6,7 +6,7 @@ function buildHeader () {
     return header;
 }
 
-// Description's Page
+// Description
 
 function builDescription () {
     const div = document.createElement('div');
@@ -27,12 +27,77 @@ function builDescription () {
     return div;
 }
 
+// Main
+
+function buildMain () {
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+    main.appendChild(container);
+    container.appendChild(buildGrey());
+    container.appendChild(buildUl());
+
+    return main;
+}
+
+// Grey
+
+function buildGrey () {
+    const div = document.createElement('div');
+    div.classList.add('greyArea');
+    
+    const p = document.createElement('p');
+    p.textContent = '558x460';
+    div.appendChild(p);
+
+
+    return div;
+}
+
+// Ul
+
+function buildUl () {
+    const div = document.createElement('div');
+    div.classList.add('textArea');
+    const ul = document.createElement('ul');
+    ul.classList.add('ul');
+    const li = document.createElement('li');
+    li.classList.add('li');
+    const h3 = document.createElement('h3');
+    h3.classList.add('h3');
+    h3.textContent = 'Our Mission';
+    const p = document.createElement('p');
+    p.classList.add('p');
+    p.textContent = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad quasi, deleniti quod voluptatibus est inventore excepturi quos recusandae molestias, voluptates ut harum corporis. Unde sint odit pariatur impedit nemo. Possimus quos, sed ea nemo quidem, beatae dolor at corrupti eum, placeat distinctio. Impedit error, sint dolorum maiores consectetur voluptas eveniet?';
+
+    li.appendChild(h3);
+    li.appendChild(p);
+    
+    div.appendChild(ul);
+    ul.appendChild(li);
+
+    return ul;
+}
+
+// Footer
+
+function buildFooter () {
+    const footer = document.createElement('footer');
+    footer.classList.add('footer');
+    footer.innerHTML = '<p>&copy; copy rights reserved to Igal</p>';
+    return footer;
+}
+
 // Make all the pieces
 function buildPage (){
     const body = document.querySelector('body');
     body.classList.add('body');
     body.appendChild(buildHeader());
     body.appendChild(builDescription());
+    body.appendChild(buildMain());
+    body.appendChild(buildFooter());
 }
 
 
